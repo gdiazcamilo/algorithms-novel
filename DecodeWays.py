b8827bee9ae9de3b9f695ec63ea4d5ad7481ae17
Gustavo Díaz <gdiazcamilo@gmail.com>
2022-08-20 08:01:11 -0400

    add comments and little refactors

--- DecodeWays.py
+++ DecodeWays.py
@@ -3,7 +3,10 @@ class Decoder:
     def get_decoding_ways(self, encoded_digits: str) -> int:
         """
             Returns an integer indicating how many possible ways the encoded
-            string can be decoded.
+            string can be decoded. 
+            
+            It uses the 26 letters of the alphabet as mapping for the digits.
+            Being A=1 and Z=26.
 
             It assumes the encoded_digits params contains only digits. 
             
@@ -22,55 +25,77 @@ class Decoder:
                 * Start with one. This is the base case for convenience.
         """
 
-        # Check the first digit outside the loop allow us to 
-        # avoid checks for out of bound indexes  or conditions that 
-        # will only apply to the first iteration.
+        # Check the first digit outside the loop allow us to avoid checks for
+        #  out of bound indexes or put conditions inside the loop that will only
+        #  apply to the first iteration.
         if not self.__first_digit_can_be_decoded(encoded_digits):
             return 0
 
-        penultimate_decoding_ways = 1
-        last_decoding_ways = 1
+        #This is the base case
+        decoding_ways_two_previous_position = 1
+        #This accounts for the number of ways we can decode the first digit, which is always one.
+        decoding_ways_previous_position = 1
 
         for previous_digit, current_digit in self.__encoded_digits(encoded_digits):
-            previous_decoding_ways = last_decoding_ways
+            previous_decoding_ways = decoding_ways_previous_position
 
-            if self.__can_not_be_decoded(previous_digit, current_digit):
+            if not self.__can_be_decoded(previous_digit, current_digit):
                 return 0
 
             if self.__digits_must_be_combined(previous_digit, current_digit):
-                last_decoding_ways = penultimate_decoding_ways
+                decoding_ways_previous_position = decoding_ways_two_previous_position
             elif self.__digits_can_be_combined_or_single(previous_digit, current_digit):
-                last_decoding_ways += penultimate_decoding_ways
+                decoding_ways_previous_position += decoding_ways_two_previous_position
             
-            penultimate_decoding_ways = previous_decoding_ways
+            decoding_ways_two_previous_position = previous_decoding_ways
         
-        return last_decoding_ways
+        return decoding_ways_previous_position
 
 
     def __first_digit_can_be_decoded(self, encoded_digits: str):
         """
-        Check if the first digit is not zero ('0').
+            Check if the first digit is not zero ('0').
 
-        Zero doesn't map to any letter in the alphabet, being A=1, B=2... Z=26.
-        Zero is only valid if it's preceded by an 1 or 2 to form a 10 or 20.
+            Note that there is no letter corresponding to zero in the mapping.
         """
 
-        first_digit = encoded_digits[0]
-        return first_digit != '0'
+        return self.__can_be_decoded(None, encoded_digits[0])
 
+
+    def __can_be_decoded(self, previous_digit, current_digit):
+        """
+            Checks if the previous and current digit combined in that order 
+            forms a number that can be mapped to any letter.
+
+            Note that for example '06' can't be mapped to any letter. It's 
+            different than '6'. All digits must be used.
+        """
+
+        # Zero doesn't map to any letter in the alphabet, being A=1, B=2... Z=26.
+        # Zero is only valid if it's preceded by an 1 or 2 to form a 10 or 20.
+        return current_digit != '0' or previous_digit in ('1', '2')
         
+
     def __encoded_digits(self, encoded_digits):
+        """
+            Yields all the digits in the received param in pair of two using a 
+            tuple (first_digit, current_digit).
+        """
+        
         for digit_idx in range(1, len(encoded_digits)):
             previous_digit = encoded_digits[digit_idx - 1]
             current_digit = encoded_digits[digit_idx]
             yield previous_digit, current_digit
 
 
-    def __can_not_be_decoded(self, previous_digit, current_digit):
-        return current_digit == '0' and previous_digit not in '12'
-
-
     def __digits_must_be_combined(self, previous_digit, current_digit):
+        """
+            Returns true if the previous and current digits combined in that 
+            same order can only be used together and not as single digits.
+        """
+
+        # If the current digit is a zero, the previous must be 1 or 2 to form a 10 or 20.
+        # Any other value for the previous digit will result in a number that's not in the mapping.
         return previous_digit in '12' and current_digit == '0' 
 
 
@@ -82,8 +107,3 @@ class Decoder:
     
         return (previous_digit == '1' or 
             (previous_digit == '2' and current_digit in '123456'))
-
-
-decoder = Decoder('121')
-print(decoder.get_decoding_ways())
-assert  decoder.get_decoding_ways() == 3
